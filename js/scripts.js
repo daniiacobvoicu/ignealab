406c475314a2e95398a0dd945e2a0374c873ae39
daniiacobvoicu <43016371+daniiacobvoicu@users.noreply.github.com>
2023-06-05 19:33:50 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function ShowandHide() {
+    var x = document.getElementById("hidden_news");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = 'none';
+    }
+}
